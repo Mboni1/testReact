7aae0e87e541571ff5e7ff03f2a5f71e91c68d6c
stana <YOUR_EMAIL>
2025-06-19 18:47:09 +0200

    test handleClick on cout

--- testReact/src/components/Button.jsx
+++ testReact/src/components/Button.jsx
@@ -1,8 +1,18 @@
 function Button(){
 
-    const handleClick = () => console.log("OUCH!");
-    const handleClick2 =(name) => console.log(`${name} stop clicking more` );
-  return(<button onClick={() =>handleClick2("bro")}> Click More</button>);
+    let count = 0;
+    const handleClick = (name) => {
+        if(count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`);
+
+
+            }
+         else{
+            console.log(`${name} stop clicking me!`);
+         }
+    };
+    return(<button onClick={() =>handleClick('bro')}>click me</button>);
 }
 
 export default Button
